fe37c2f721e8aec01a71cba5cda920d0d72610ff
MIgimimi2mm <kanzengyoumuyou0331@gmail.com>
2024-06-26 11:30:43 +0900

    デモ版UI向上

--- pages/directory/photo.js
+++ pages/directory/photo.js
@@ -1,95 +1,48 @@
-
-
 "use client";
 import React from "react";
 import Header from '/components/Header.js'
 import Image from 'next/image';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import Icon_bar from '../../components/Icon_bar';
-
-
 
 function Photo() {
     return (
         <div className="font-sans mx-auto">
             <Header />
             <div className="flex justify-center min-h-screen bg-[#547443] p-6 text-white ">
-                <motion.div
-                    viewport={{ once: true }}
-                    initial={{ opacity: 0 }}
+                <div className="w-full">
+                    <motion.div
+                        key="photo-page"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 0.4, delay: 0.2, duration: 0.5 }}
+                    >
 
-                    exit={{ opacity: 0 }}
-                    whileInView={{ opacity: 1 }}
-                    transition={{ duration: 0.4, delay: 0.2, ease: "easeOut" }} >
-                    <Icon_bar />
-                    <div className="grid grid-cols-2 gap-4 rounded">
-                        <Image
-                            src="/images/photo (1).jpg"
-                            alt="Description of image 1"
-                            width={1000}
-                            height={760}
-                            className="w-full h-auto rounded-lg"
-                        />
-                        <Image
-                            src="/images/photo (2).jpg"
-                            alt="Description of image 2"
-                            width={1000}
-                            height={760}
-                            className="w-full h-auto rounded-lg"
-                        />
-                        <Image
-                            src="/images/photo (3).jpg"
-                            alt="Description of image 3"
-                            width={1000}
-                            height={760}
-                            className="w-full h-auto rounded-lg"
-                        />
-                        <Image
-                            src="/images/photo (4).jpg"
-                            alt="Description of image 4"
-                            width={1000}
-                            height={760}
-                            className="w-full h-auto rounded-lg"
-                        />
-                        <Image
-                            src="/images/photo (5).jpg"
-                            alt="Description of image 4"
-                            width={1000}
-                            height={760}
-                            className="w-full h-auto rounded-lg"
-                        />
-                        <Image
-                            src="/images/photo (6).jpg"
-                            alt="Description of image 4"
-                            width={1000}
-                            height={760}
-                            className="w-full h-auto rounded-lg"
-                        />
-                        <Image
-                            src="/images/photo (7).jpg"
-                            alt="Description of image 4"
-                            width={1000}
-                            height={760}
-                            className="w-full h-auto rounded-lg"
-                        />
-                        <Image
-                            src="/images/photo (8).jpg"
-                            alt="Description of image 4"
-                            width={1000}
-                            height={760}
-                            className="w-full h-auto rounded-lg"
-                        />
-                        <Image
-                            src="/images/photo (9).jpg"
-                            alt="Description of image 4"
-                            width={1000}
-                            height={760}
-                            className="w-full h-auto rounded-lg"
-                        />
-                    </div>
-                </motion.div>
+                        <Icon_bar />
+                        <div className="grid grid-cols-2 gap-4 rounded">
+                            {['photo (1).jpg', 'photo (2).jpg', 'photo (3).jpg', 'photo (4).jpg', 'photo (5).jpg', 'photo (6).jpg', 'photo (7).jpg', 'photo (8).jpg', 'photo (9).jpg'].map((src, index) => (
+                                <motion.div
+                                    key={index}
+                                    viewport={{ once: true }}
+                                    initial={{ opacity: 0 }}
+                                    whileInView={{ opacity: 1 }}
+                                    transition={{ duration: 0.4, delay: 0.2 * index, ease: "easeOut" }}
+                                >
+                                    <Image
+                                        src={`/images/${src}`}
+                                        alt={`Description of image ${index + 1}`}
+                                        width={1000}
+                                        height={760}
+                                        className="w-full h-auto rounded-lg"
+                                    />
+                                </motion.div>
+                            ))}
+                        </div>
+
+                    </motion.div>
+                </div>
             </div>
-
         </div>
     );
 }
--- pages/directory/thanks.js
+++ pages/directory/thanks.js
@@ -1,4 +1,6 @@
 import Header from '/components/Header.js'
+import { motion } from 'framer-motion';
+
 
 
 export default function Thanks() {
@@ -10,11 +12,21 @@ export default function Thanks() {
 
                 <Header />
 
-                <div className="flex justify-center min-h-screen bg-[#547443] p-6 text-white ">
 
-                    <h1 className="text-4xl font-bold mb-4 text-center">
-                        水やりありがとちゃ～ん♡
-                    </h1>
+                <div className="flex justify-center min-h-screen bg-[#547443] p-6 text-white ">
+                    <motion.div
+                        key="photo-page"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 0.4, delay: 0.2, duration: 0.5 }}
+                    >
+
+                        <h1 className="text-4xl font-bold mb-4 text-center">
+                            水やりありがとちゃ～ん♡
+                        </h1>
+                    </motion.div>
+
                 </div>
             </div>
 
