2a43892a1a996897a11918af66d59484537ceede
MIgimimi2mm <kanzengyoumuyou0331@gmail.com>
2024-06-26 11:32:58 +0900

    デモ版UI向上2

--- pages/index.js
+++ pages/index.js
@@ -78,7 +78,7 @@ export default function Home() {
             viewport={{ once: true }}
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
-            transition={{ duration: 0.4, delay: 0.4, ease: "easeOut" }}
+            transition={{ duration: 0.4, delay: 0.2, ease: "easeOut" }}
           >
             <Garden />
 
@@ -88,7 +88,7 @@ export default function Home() {
             viewport={{ once: true }}
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
-            transition={{ duration: 0.4, delay: 0.6, ease: "easeOut" }}
+            transition={{ duration: 0.4, delay: 0.2, ease: "easeOut" }}
           >
             <Shikumi />
 
